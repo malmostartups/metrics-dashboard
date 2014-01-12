ed3168085ff7ba17df7f42df8e1b6499956d76b6
Joel Larsson <tilljoel@gmail.com>
2014-01-12 19:51:12 +0100

    Update: add twitter followers, tweets, etc

---
+++ jobs/twitter_user.rb
@@ -0,0 +1,36 @@
+#### Get your twitter keys & secrets:
+#### https://dev.twitter.com/docs/auth/tokens-devtwittercom
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_OAUTH_SECRET']
+end
+
+twitter_username = 'tilljoel'
+
+MAX_USER_ATTEMPTS = 10
+user_attempts = 0
+
+SCHEDULER.every '10m', :first_in => 0 do |job|
+  begin
+    tw_user = client.user("#{twitter_username}")
+    if tw_user
+        tweets = tw_user.statuses_count
+        followers = tw_user.followers_count
+        following = tw_user.friends_count
+
+        send_event('twitter_user_tweets', current: tweets)
+        send_event('twitter_user_followers', current: followers)
+        send_event('twitter_user_following', current: following)
+
+    end
+  rescue Twitter::Error => e
+    user_attempts = user_attempts +1
+    puts "Twitter error #{e}"
+    puts "\e[33mFor the twitter_user widget to work, you need to put in your twitter API keys in the jobs/twitter_user.rb file.\e[0m"
+    sleep 5
+    retry if(user_attempts < MAX_USER_ATTEMPTS)
+  end
+end
